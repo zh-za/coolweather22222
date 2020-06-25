9c96625b3756e12761fc0e999f78a04d0264b572
zh-za <29596472382@qq.com>
2020-06-25 09:48:50 +0800

    first commit

---
+++ app/src/main/java/com/hy/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.hy.coolweather.gson;
+
+public class AQI {
+    public AQICity city;    // 城市
+
+       	    public class AQICity{
+
+        	        public String aqi;  // 空气质量指数
+	        public String pm25; // pm2.5浓度
+
+        	    }
+
+}
---
+++ app/src/main/java/com/hy/coolweather/util/Utility.java
@@ -0,0 +1,99 @@
+package com.hy.coolweather.util;
+
+import android.text.TextUtils;
+
+import com.google.gson.Gson;
+import com.hy.coolweather.db.City;
+import com.hy.coolweather.db.Country;
+import com.hy.coolweather.db.Province;
+import com.hy.coolweather.gson.Weather;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+
+	/**
+	 * 解析和处理服务器返回的省级数据
+	 */
+	public static boolean handleProvinceResponse(String response) {
+		if (!TextUtils.isEmpty(response)) {  // 数据非空
+			try {
+				JSONArray allProvinces = new JSONArray(response);   // 创建一个用于解析的 JSON 数组
+				for (int i = 0; i < allProvinces.length(); i++) {
+					JSONObject provinceObject = allProvinces.getJSONObject(i);  // 获取每一个 JSON 对象
+					Province province = new Province();
+					province.setProvinceName(provinceObject.getString("name")); // 将 JSON 对象中的 name 字段赋给 ProvinceName
+					province.setProvinceCode(provinceObject.getInt("id"));  // 将 JSON 对象中的 id 字段赋给 ProvinceCode
+					province.save(); // 将数据存到数据库中
+				}
+				return true;
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	/**
+	 * 解析和处理服务器返回的市级数据
+	 */
+	public static boolean handleCityResponse(String response, int provinceId) {
+		if (!TextUtils.isEmpty(response)) {
+			try {
+				JSONArray allCity = new JSONArray(response);
+				for (int i = 0; i < allCity.length(); i++) {
+					JSONObject cityObject = allCity.getJSONObject(i);
+					City city = new City();
+					city.setCityName(cityObject.getString("name"));
+					city.setCityCode(cityObject.getInt("id"));
+					city.setProvinceId(provinceId);
+					city.save();
+				}
+				return true;
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	/**
+	 * 解析和处理服务器返回的县级数据
+	 */
+	public static boolean handleCountyResponse(String response, int cityId) {
+		if (!TextUtils.isEmpty(response)) {
+			try {
+				JSONArray allCounties = new JSONArray(response);
+				for (int i = 0; i < allCounties.length(); i++) {
+					JSONObject countyObject = allCounties.getJSONObject(i);
+					Country county = new Country();
+					county.setCountryName(countyObject.getString("name"));
+					county.setWeatherId(countyObject.getString("weather_id"));
+					county.setCityId(cityId);
+					county.save();
+				}
+				return true;
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 将返回的JSON数据解析成Weather实体类
+	 */
+	public static Weather handleWeatherResponse(String response){
+		try{
+			JSONObject jsonObject = new JSONObject(response);
+			JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+			String weatherContent = jsonArray.getJSONObject(0).toString();
+			return new Gson().fromJson(weatherContent, Weather.class);  // 将JSON数据解析成Weather对象
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
+
